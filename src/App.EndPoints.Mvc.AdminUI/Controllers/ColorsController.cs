7309117797ae37d871195321f9f1ba22b8ca71e9
amir asadpour <amirmohammad.asadpoor@gmail.com>
2022-05-29 19:17:33 +0430

    Add CRUD for Colors

--- src/App.EndPoints.Mvc.AdminUI/Controllers/ColorsController.cs
+++ src/App.EndPoints.Mvc.AdminUI/Controllers/ColorsController.cs
@@ -4,7 +4,27 @@ namespace App.EndPoints.Mvc.AdminUI.Controllers
 {
     public class ColorsController : Controller
     {
-        public IActionResult Colors()
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        
+        public IActionResult Read()
+        {
+            return View();
+        }
+        
+        public IActionResult Update()
         {
             return View();
         }
