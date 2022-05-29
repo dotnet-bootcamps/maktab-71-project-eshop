d6296356592764299598a2340998bb5589abc610
amir asadpour <amirmohammad.asadpoor@gmail.com>
2022-05-29 18:38:28 +0430

    add color controller

---
+++ src/App.EndPoints.Mvc.AdminUI/Controllers/ColorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.EndPoints.Mvc.AdminUI.Controllers
+{
+    public class ColorsController : Controller
+    {
+        public IActionResult Colors()
+        {
+            return View();
+        }
+    }
+}
