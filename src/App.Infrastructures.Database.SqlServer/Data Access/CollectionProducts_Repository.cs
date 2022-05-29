bb23b430b897c285f7ccff7430a2cc66f0532441
mohsen-test <alipourmohsen2015@gmail.com>
2022-05-29 18:59:10 +0430

    collection nd product collection repository

---
+++ src/App.Infrastructures.Database.SqlServer/Data Access/CollectionProducts_Repository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class CollectionProducts_Repository
+{
+    private AppDbContext _shopDB;
+
+    public CollectionProducts_Repository()
+    {
+        _shopDB = new AppDbContext();
+    }
+    public void AddCollectionProducts(CollectionProducts item)
+    {
+        _shopDB.CollectionProducts.Add(item);
+        _shopDB.SaveChanges();
+
+    }
+
+    public List<CollectionProducts> GetAllCollectionProducts()
+    {
+        return _shopDB.CollectionProducts.ToList();
+    }
+
+
+    public void DelteCollectionProducts(CollectionProducts item)
+    {
+        _shopDB.CollectionProducts.Remove(item);
+        _shopDB.SaveChanges();
+    }
+}
