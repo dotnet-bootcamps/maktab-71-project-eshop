dbaa69cdd883b09f75c70defb619aa0c89ff2c53
raspinagamestudio99@gmail.com <raspinagamestudio99@gmail.com>
2022-05-29 18:55:58 +0430

    Tag Repository Changed.

--- src/App.Infrastructures.Database.SqlServer/Repositories/TagRepository.cs
+++ src/App.Infrastructures.Database.SqlServer/Repositories/TagRepository.cs
@@ -9,32 +9,32 @@ using System.Threading.Tasks;
 
 namespace App.Infrastructures.Database.SqlServer.Repositories
 {
-    internal class TagRepository
+    public class TagRepository
     {
-        public static AppDbContext _eshop = new();
-        public static void Create(Tag tag)
+        public AppDbContext _eshop = new();
+        public void Create(Tag tag)
         {
             _eshop.Tags.Add(tag);
             _eshop.SaveChanges();
         }
-        public static void Edit(Tag tag)
+        public void Edit(Tag tag)
         {
             var _tag = _eshop.Tags.First(p => p.Id == tag.Id);
             _tag.Name = tag.Name;
             _tag.CreationDate = tag.CreationDate;
             _eshop.SaveChanges();
         }
-        public static void Delete(int id)
+        public void Delete(int id)
         {
             var _tag = _eshop.Tags.First(p => p.Id == id);
             _eshop.Tags.Remove(_tag);
             _eshop.SaveChanges();
         }
-        public static List<Tag> GetAll()
+        public List<Tag> GetAll()
         {
             return _eshop.Tags.Include(b => b.ProductTags).ToList();
         }
-        public static Tag GetById(int id)
+        public Tag GetById(int id)
         {
             return _eshop.Tags.First(p => p.Id == id);
         }
