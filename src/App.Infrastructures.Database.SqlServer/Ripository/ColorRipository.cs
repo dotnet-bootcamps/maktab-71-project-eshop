abcfcfd7f21aac8c3456c35c5f9827d16b2f3fb1
radin3607 <radin3607@yahoo.com>
2022-05-29 07:23:02 -0700

    imanelahy3

--- src/App.Infrastructures.Database.SqlServer/Ripository/ColorRipository.cs
+++ src/App.Infrastructures.Database.SqlServer/Ripository/ColorRipository.cs
@@ -12,13 +12,13 @@ namespace App.Infrastructures.Database.SqlServer.Ripository
     public class ColorRipository
     {
 
-        public static AppDbContext _eshop = new();
+        public  AppDbContext _eshop = new();
         public static void Create(Color color)
         {
             _eshop.Colors.Add(color);
             _eshop.SaveChanges();
         }
-        public static void Edit(Color model)
+        public  void Edit(Color model)
         {
             var color = _eshop.Colors.First(p => p.Id == model.Id);
             color.Name = model.Name;
@@ -26,7 +26,7 @@ namespace App.Infrastructures.Database.SqlServer.Ripository
             color.CreationDate = model.CreationDate;
             _eshop.SaveChanges();
         }
-        public static void Delete(int id)
+        public  void Delete(int id)
         {
             var color = _eshop.Colors.First(p => p.Id == id);
             _eshop.Colors.Remove(color);
@@ -34,13 +34,13 @@ namespace App.Infrastructures.Database.SqlServer.Ripository
         }
 
 
-        public static List GetAll()
+        public  List GetAll()
         {
             return _eshop.Colors.Include(b => b.ProductColors).ToList();
         }
-        public static Brand GetById(int id)
+        public  Color GetById(int id)
         {
-            return _eshop.Brands.First(p => p.Id == id);
+            return _eshop.Colors.First(p => p.Id == id);
         }
     }
 }
