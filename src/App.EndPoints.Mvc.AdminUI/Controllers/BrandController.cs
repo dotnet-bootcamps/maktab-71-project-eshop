5c2ee2ed7477a60fd695199f63e034d8c5fc07b1
Khaspool <47502089+Khaspool@users.noreply.github.com>
2022-05-29 18:29:16 +0430

    Update BrandController.cs

--- src/App.EndPoints.Mvc.AdminUI/Controllers/BrandController.cs
+++ src/App.EndPoints.Mvc.AdminUI/Controllers/BrandController.cs
@@ -8,5 +8,42 @@ namespace App.EndPoints.Mvc.AdminUI.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string model)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update(string model)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(string model)
+        {
+            return View();
+        }
     }
 }
