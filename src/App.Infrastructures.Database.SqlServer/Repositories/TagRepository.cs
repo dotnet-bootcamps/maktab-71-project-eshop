03965be0fecfab088c32a549e5d260d49ecd3b77
raspinagamestudio99@gmail.com <raspinagamestudio99@gmail.com>
2022-05-31 18:31:28 +0430

    ITagRepsitory Implemented.

--- src/App.Infrastructures.Database.SqlServer/Repositories/TagRepository.cs
+++ src/App.Infrastructures.Database.SqlServer/Repositories/TagRepository.cs
@@ -1,5 +1,6 @@
 ﻿using App.Infrastructures.Database.SqlServer.Data;
 using App.Infrastructures.Database.SqlServer.Entities;
+using App.Infrastructures.Database.SqlServer.Repositories.Contract;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace App.Infrastructures.Database.SqlServer.Repositories
 {
-    public class TagRepository
+    public class TagRepository : ITagRepository
     {
         public AppDbContext _eshop = new();
         public void Create(Tag tag)
@@ -17,14 +18,14 @@ namespace App.Infrastructures.Database.SqlServer.Repositories
             _eshop.Tags.Add(tag);
             _eshop.SaveChanges();
         }
-        public void Edit(Tag tag)
+        public void Update(Tag tag)
         {
             var _tag = _eshop.Tags.First(p => p.Id == tag.Id);
             _tag.Name = tag.Name;
             _tag.CreationDate = tag.CreationDate;
             _eshop.SaveChanges();
         }
-        public void Delete(int id)
+        public void Remove(int id)
         {
             var _tag = _eshop.Tags.First(p => p.Id == id);
             _eshop.Tags.Remove(_tag);
