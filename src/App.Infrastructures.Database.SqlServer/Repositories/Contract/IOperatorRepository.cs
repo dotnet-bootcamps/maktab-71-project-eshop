8a930c0209a44ddf7f3671d12058d6e0c52ef3a7
m.parsa <parsa.itsu@gmail.com>
2022-05-31 19:02:32 +0430

    ParsaAddedOperatorRepository

---
+++ src/App.Infrastructures.Database.SqlServer/Repositories/Contract/IOperatorRepository.cs
@@ -0,0 +1,18 @@
+﻿using App.Infrastructures.Database.SqlServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infrastructures.Database.SqlServer.Repositories.Contract
+{
+    public interface IOperatorRepository
+    {
+        List<Operator> GetAll();
+        Operator GetById(int id);
+        void Add(Operator @operator);
+        void Remove(int id);
+        
+    }
+}
