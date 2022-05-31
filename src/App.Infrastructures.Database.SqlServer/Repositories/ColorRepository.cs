5449230da187c4672af14d6fd8284be597462bbf
siamak <siamakanzabi@gmail.com>
2022-05-31 18:25:49 +0430

    Add IColorRepoitory

--- src/App.Infrastructures.Database.SqlServer/Repositories/ColorRepository.cs
+++ src/App.Infrastructures.Database.SqlServer/Repositories/ColorRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace App.Infrastructures.Database.SqlServer.Ripository
 {
-    public class ColorRepository
+    public class ColorRepository : IColorRepository
     {
 
         public AppDbContext _eshop;
@@ -19,28 +19,8 @@ namespace App.Infrastructures.Database.SqlServer.Ripository
             _eshop = new AppDbContext();
         }
 
-        public  void Create(App.Infrastructures.Database.SqlServer.Entities.Color color)
-        {
-            _eshop.Colors.Add(color);
-            _eshop.SaveChanges();
-        }
-        public  void Edit(App.Infrastructures.Database.SqlServer.Entities.Color model)
-        {
-            var color = _eshop.Colors.First(p => p.Id == model.Id);
-            color.Name = model.Name;
-            color.Code = model.Code;
-            color.CreationDate = model.CreationDate;
-            _eshop.SaveChanges();
-        }
-        public  void Delete(int id)
-        {
-            var color = _eshop.Colors.First(p => p.Id == id);
-            _eshop.Colors.Remove(color);
-            _eshop.SaveChanges();
-        }
 
-
-        public  List<App.Infrastructures.Database.SqlServer.Entities.Color> GetAll()
+        public List<App.Infrastructures.Database.SqlServer.Entities.Color> GetAll()
         {
             return _eshop.Colors.Include(b => b.ProductColors).ToList();
         }
@@ -48,5 +28,27 @@ namespace App.Infrastructures.Database.SqlServer.Ripository
         {
             return _eshop.Colors.First(p => p.Id == id);
         }
+
+        public  void Add(App.Infrastructures.Database.SqlServer.Entities.Color color)
+        {
+            _eshop.Colors.Add(color);
+            _eshop.SaveChanges();
+        }
+        public  void Update(App.Infrastructures.Database.SqlServer.Entities.Color model)
+        {
+            var color = _eshop.Colors.First(p => p.Id == model.Id);
+            color.Name = model.Name;
+            color.Code = model.Code;
+            color.CreationDate = model.CreationDate;
+            _eshop.SaveChanges();
+        }
+        public  void Remove(int id)
+        {
+            var color = _eshop.Colors.First(p => p.Id == id);
+            _eshop.Colors.Remove(color);
+            _eshop.SaveChanges();
+        }
+
+
     }
 }
---
+++ src/App.Infrastructures.Database.SqlServer/Repositories/Contract/IColorRepository.cs
@@ -0,0 +1,11 @@
+﻿using App.Infrastructures.Database.SqlServer.Entities;
+
+public interface IColorRepository
+{
+    List<Color> GetAll();
+    Color GetById(int id);
+    void Add(Color color);
+    void Update(Color color);
+    void Remove(int id);
+
+}
