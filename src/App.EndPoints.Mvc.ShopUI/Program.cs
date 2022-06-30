dc15825788aeb2d744f12a821150ac339706993d
SadraDN <daviransadra@gmail.com>
2022-07-01 00:28:47 +0430

    Changed Add Color UI-Add Product UI Changed-Add product pic method

--- src/App.EndPoints.Mvc.ShopUI/Program.cs
+++ src/App.EndPoints.Mvc.ShopUI/Program.cs
@@ -35,8 +35,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews()
-    .AddRazorRuntimeCompilation();
+builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<AppDbContext>(option =>
 {
     option.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB; Initial Catalog=DotNetShopDb; Integrated Security=TRUE");
