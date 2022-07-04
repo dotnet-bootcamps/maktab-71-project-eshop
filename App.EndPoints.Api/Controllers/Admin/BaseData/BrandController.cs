b815e1285d3ef1d4b8a335852bbb514efee8ffed
Amirghk <ghkamirhossein@gmail.com>
2022-07-04 23:52:01 +0430

    added post api

---
+++ App.EndPoints.Api/Controllers/Admin/BaseData/BrandController.cs
@@ -0,0 +1,34 @@
+﻿using App.Domain.Core.BaseData.Contarcts.AppServices;
+using App.Domain.Core.BaseData.Dtos;
+using App.EndPoints.Api.Attributes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.EndPoints.Api.Controllers.Admin.BaseData
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IBrandAppService _brandAppService;
+        public BrandController(IBrandAppService brandAppService)
+        {
+            _brandAppService = brandAppService;
+        }
+
+        [HttpGet]
+        [ApiKeyAuthorize]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _brandAppService.GetAll());
+        }
+
+        [HttpPost]
+        [ApiKeyAuthorize]
+        public async Task<IActionResult> Post(string name, int displayOrder, CancellationToken cancellationToken)
+        {
+            await _brandAppService.Set(name, displayOrder);
+            return Ok();
+        }
+    }
+}
--- src/App.EndPoints.Mvc.ShopUI/Areas/Admin/Controllers/BrandController.cs
+++ src/App.EndPoints.Mvc.ShopUI/Areas/Admin/Controllers/BrandController.cs
@@ -4,8 +4,8 @@ using App.Domain.Core.BaseData.Dtos;
 using App.EndPoints.Mvc.AdminUI.Models.ViewModels.BaseData.Brand;
 using App.EndPoints.Mvc.AdminUI.Models.ViewModels.Product;
 using Microsoft.AspNetCore.Mvc;
-
-
+using Newtonsoft.Json;
+using System.Text;
 
 namespace App.EndPoints.Mvc.AdminUI.Controllers
 {
@@ -14,11 +14,13 @@ namespace App.EndPoints.Mvc.AdminUI.Controllers
     {
         private readonly IBrandAppService _brandAppService;
         private readonly IBrandService _brandService;
+        private readonly IConfiguration _configuration;
 
-        public BrandController(IBrandAppService brandAppService, IBrandService brandService)
+        public BrandController(IBrandAppService brandAppService, IBrandService brandService, IConfiguration configuration)
         {
             _brandAppService = brandAppService;
             _brandService = brandService;
+            _configuration = configuration;
         }
 
         public async Task<IActionResult> Index()
@@ -42,16 +44,41 @@ namespace App.EndPoints.Mvc.AdminUI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(BrandAddViewModel brand)
+        public async Task<IActionResult> Create(BrandAddViewModel brand, CancellationToken cancellationToken)
         {
-            //if (ModelState.IsValid && brand.Name.ToLower() == "hp" && brand.DisplayOrder > 2)
-            //    ModelState.AddModelError("", "برند اچ پی باید در ابتدای لیست قرار بگیرد");
             if (!ModelState.IsValid)
             {
                 return View(brand);
             }
-            await _brandAppService.Set(brand.Name, brand.DisplayOrder);
-            return RedirectToAction("");
+
+            //var client = new HttpClient();
+            //var jsonContent = JsonConvert.SerializeObject(dto);
+            //HttpContent httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            //client.DefaultRequestHeaders.Add("ApiKey", _configuration.GetSection("ApiKey").Value);
+            //var httpResponse = await client.PostAsync("https://localhost:7137/api/Product/SetProduct", httpContent, cancellationToken);
+            //if (!httpResponse.IsSuccessStatusCode)
+            //{
+            //    throw new Exception("خطایی در دریافت اطلاعات رخ داد.");
+            //}
+            new Dictionary<string, string>()
+            {
+                {"name" , brand.Name},
+                {"displayOrder", brand.DisplayOrder.ToString()}
+            };
+
+
+            var client = new HttpClient();
+            var jsonContent = JsonConvert.SerializeObject(brand);
+            var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            client.DefaultRequestHeaders.Add("ApiKey", _configuration.GetSection("ApiKey").Value);
+            var response = await client.PostAsync(@"https://localhost:7137/api/Brand", httpContent, cancellationToken);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(response.StatusCode.ToString());
+            }
+
+            return RedirectToAction(nameof(Index));
+            // await _brandAppService.Set(brand.Name, brand.DisplayOrder);
         }
 
         [HttpGet]
